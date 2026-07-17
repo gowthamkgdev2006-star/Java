af889d31a8a550882a522a528d94b3ddbb15c4e5
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-07-17 21:06:25 +0530

    Set square size to a fixed value of 5
    Replaced user input for square size with a fixed value of 5.

--- Patten Problems/Square.java
+++ Patten Problems/Square.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
 public class Square
 {
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of the square: ");
-        int n = sc.nextInt();
+        int n = 5;
         
         for (int i = 0; i < n; i++)
         {
