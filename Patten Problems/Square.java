3e96807f1c3429fc3a725d3a4be69674ddf76473
GOWTHAM K <gowthamkgdev2006@gmail.com>
2026-07-17 21:03:43 +0530

    Add Square.java to print asterisks square
    Implement a Java program to print a square of asterisks based on user input.

---
+++ Patten Problems/Square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Square
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square: ");
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++)
+        {
+            System.out.println("");
+            for (int j = 0; j < n; j++)
+            {
+                System.out.print("* ");
+            }
+        }
+    }
+}
